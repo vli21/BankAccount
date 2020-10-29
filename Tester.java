1bc0e99500916599d8b878ad43a85fa4e1ee09e7
vli21 <vli21@stuy.edu>
2020-10-28 22:11:24 -0400

    MY tester tests all my methods and they work

---
+++ Tester.java
@@ -0,0 +1,26 @@
+public class Tester{
+  public static void main(String[]args){
+
+    BankAccount mine = new BankAccount (1234567, "redorangeyellow");
+
+     System.out.println (mine.toString());
+
+     System.out.println (mine.getBalance());
+
+     mine.deposit(1234.69);
+
+     System.out.println (mine.getBalance());
+
+     mine.withdraw (100.00);
+
+     System.out.println (mine.getBalance());
+
+     System.out.println (mine.getAccountID());
+
+     System.out.println (mine.getpassword());
+
+     mine.setPassword("greenbluepurple");
+
+     System.out.println (mine.getpassword());
+  }
+}
