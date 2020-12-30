476cfac70c2cd3d23821778c7bbd10a4a7f438e0
vli21 <vli21@stuy.edu>
2020-12-30 18:13:18 -0500

    fixed broken constructor

--- BankAccount.java
+++ BankAccount.java
@@ -3,10 +3,10 @@ public class BankAccount{
   private int accountID;
   private String password;
 
-  public BankAccount (int accountID, String password){
-    this.accountID = accountID;
-    this.password = password;
-    balance=  0.0;
+  public BankAccount (int ID, String pass){
+    accountID = ID;
+    password = pass;
+    balance= 0.0;
   }
 
   public double getBalance (){
@@ -21,30 +21,45 @@ public class BankAccount{
   }
 
   public boolean deposit(double amount){
-    if (amount < 0 ){
-      return false;
-    }
-    else {
+    if (amount > 0 ){
       balance+= amount;
       return true;
+    }
+    else {
+      return false;
     }
   }
 
   public boolean withdraw(double amount){
-    if (amount > balance && amount < 0){
+    if (amount > this.balance || amount < 0){
       return false;
     }
     else {
-      balance -= amount;
+      this.balance -= amount;
       return true;
     }
   }
 
   public String toString(){
-    return (accountID+"/t"+balance);
+    return ("#"+accountID+"/t"+"$"+balance);
   }
 
    public void setPassword(String newPass){
      password= newPass;
    }
+   private boolean authenticate (String password){
+     return (this.password.equals(password));
+   }
+
+   public boolean transferTo(BankAccount other, double amount, String password){
+     if (authenticate(password) && withdraw(amount)){
+       other.deposit (amount);
+       return true;
+     }
+     else {
+       return false;
+     }
+
+     }
+
 }
